7ff549c8c6c41a68f1d63fa7e874fcd338125158
Stefan Sedich <stefan.sedich@gmail.com>
2017-01-06 08:47:43 -0800

    add plug to repo for easier install

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,3 @@
 # Install oh-my-zsh and the powerlevel9k theme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-# Install plug for vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
