46538d9d510ab976b8d05415ae5fb00728072ced
Stefan Sedich <stefan.sedich@gmail.com>
2017-02-01 17:59:34 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,7 @@
 # Install oh-my-zsh and the powerlevel9k theme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Setup neovim symlinks to vim config
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
