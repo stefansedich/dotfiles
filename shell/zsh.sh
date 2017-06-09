7312d26d8bfdf8e4b358e9f42329972d4a8c075a
Stefan Sedich <stefan.sedich@gmail.com>
2017-06-09 15:07:10 -0700

    Create zsh.sh

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -8,7 +8,7 @@ POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
 
 # oh-my-zsh
-plugins=(docker git bundler osx rake ruby)
+plugins=(docker docker-compose git bundler osx rake ruby)
 
 test -e "${ZSH}/oh-my-zsh.sh" && source "${ZSH}/oh-my-zsh.sh"
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
