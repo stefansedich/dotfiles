4e7f7f82f9594b03a1ea15eadce454cedb436e5e
Stefan Sedich <stefan.sedich@gmail.com>
2018-06-07 10:28:54 -0700

    Update zsh.sh

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,14 +1,16 @@
-ZSH=$HOME/.oh-my-zsh
-TERM="xterm-256color"
-ZSH_THEME="powerlevel9k/powerlevel9k"
-COMPLETION_WAITING_DOTS="true"
+if [ -n "$ZSH_VERSION" ]; then
+  ZSH=$HOME/.oh-my-zsh
+  TERM="xterm-256color"
+  ZSH_THEME="powerlevel9k/powerlevel9k"
+  COMPLETION_WAITING_DOTS="true"
 
-# Powerlevel9k config
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
+  # Powerlevel9k config
+  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)
+  POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
 
-# oh-my-zsh
-plugins=(docker docker-compose git bundler osx rake ruby)
+  # oh-my-zsh
+  plugins=(docker docker-compose git bundler osx rake ruby)
 
-test -e "${ZSH}/oh-my-zsh.sh" && source "${ZSH}/oh-my-zsh.sh"
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+  test -e "${ZSH}/oh-my-zsh.sh" && source "${ZSH}/oh-my-zsh.sh"
+  test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+fi
