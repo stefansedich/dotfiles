23740e989feb421f9a9dc32e76c38f2d98173c88
Stefan Sedich <stefan.sedich@gmail.com>
2019-02-28 13:53:15 -0800

    Update zsh.sh

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -6,10 +6,10 @@ if [ -n "$ZSH_VERSION" ]; then
 
   # Powerlevel9k config
   POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)
-  POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status kubecontext history time)
+  POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
 
   # oh-my-zsh
-  plugins=(docker docker-compose git bundler rake ruby kubectl)
+  plugins=(docker docker-compose git bundler rake ruby kubectl kube-ps1)
 
   test -e "${ZSH}/oh-my-zsh.sh" && source "${ZSH}/oh-my-zsh.sh"
   test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
