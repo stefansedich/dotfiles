f501a5602c0fd0583864f3ec538d0a4a0990cc4d
Stefan Sedich <stefan.sedich@gmail.com>
2019-03-12 20:19:24 -0700

    Update kubernetes.sh

--- shell/kubernetes.sh
+++ shell/kubernetes.sh
@@ -2,4 +2,9 @@ export PATH="$PATH:$HOME/.krew/bin"
 
 kube-context() {
   export KUBECONFIG="$HOME/.kube/$1"
+  
+  echo "Kube config pointing to $HOME/.kube/$1"
 }
+
+alias k='kubectl'
+alias kc='kube-context'
