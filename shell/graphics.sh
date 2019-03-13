d8d3075b9c942e7f11fd7a7c2e83a23aa263661a
Stefan Sedich <stefan.sedich@gmail.com>
2019-03-12 20:54:36 -0700

    Update graphics.sh

--- shell/graphics.sh
+++ shell/graphics.sh
@@ -1,17 +1,9 @@
 discrete() {
+  sudo prime-select nvidia
   killall Xorg
-  modprobe nvidia_drm
-  modprobe nvidia_modeset
-  modprobe nvidia
-  tee /proc/acpi/bbswitch <<<ON
-  cp /etc/X11/xorg.conf.nvidia /etc/X11/xorg.conf
 }
 
 integrated() {
+  sudo prime-select intel
   killall Xorg
-  rmmod nvidia_drm
-  rmmod nvidia_modeset
-  rmmod nvidia
-  tee /proc/acpi/bbswitch <<<OFF
-  cp /etc/X11/xorg.conf.intel /etc/X11/xorg.conf
 }
