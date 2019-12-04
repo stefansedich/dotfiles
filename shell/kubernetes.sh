fefb4ceaa1af44429fe54c213672c3d5fc991a7e
Stefan Sedich <stefan.sedich@gmail.com>
2019-12-03 16:34:22 -0800

    Update kubernetes.sh

--- shell/kubernetes.sh
+++ shell/kubernetes.sh
@@ -1,9 +1,17 @@
 export PATH="$PATH:$HOME/.krew/bin:/usr/local/kubebuilder/bin"
 
 kube-context() {
+  if [ -f "$HOME/.aws/config" ]
+  then
+    if cat ~/.aws/config | grep -q "\[profile $1\]"
+    then
+      export AWS_PROFILE="$1"
+      echo "Setting AWS_PROFILE=$1"
+    fi
+  fi
+
   export KUBECONFIG="$HOME/.kube/$1"
-  
-  echo "Kube config pointing to $HOME/.kube/$1"
+  echo "Setting KUBECONFIG=$HOME/.kube/$1"
 }
 
 alias k='kubectl'
