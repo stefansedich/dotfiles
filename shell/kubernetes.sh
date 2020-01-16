58bed611b47561795a86d4cf7322bc4cd1561f94
Stefan Sedich <stefan.sedich@gmail.com>
2020-01-16 10:39:16 -0800

    Update kubernetes.sh

--- shell/kubernetes.sh
+++ shell/kubernetes.sh
@@ -1,15 +1,6 @@
 export PATH="$PATH:$HOME/.krew/bin:/usr/local/kubebuilder/bin"
 
 kube-context() {
-  if [ -f "$HOME/.aws/config" ]
-  then
-    if cat ~/.aws/config | grep -q "\[profile $1\]"
-    then
-      export AWS_PROFILE="$1"
-      echo "Setting AWS_PROFILE=$1"
-    fi
-  fi
-
   export KUBECONFIG="$HOME/.kube/$1"
   echo "Setting KUBECONFIG=$HOME/.kube/$1"
 }
