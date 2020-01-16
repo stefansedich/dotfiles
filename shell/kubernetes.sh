6bd551dd9f2914549aca44b28eca76a0fb08284c
Stefan Sedich <stefan.sedich@gmail.com>
2020-01-16 10:53:05 -0800

    Update kubernetes.sh

--- shell/kubernetes.sh
+++ shell/kubernetes.sh
@@ -8,10 +8,3 @@ kube-context() {
 alias k='kubectl'
 alias kc='kube-context'
 alias kustomize='kubectl kustomize'
-
-# Comletion helper for the kube-context command.
-_kube_context_completions() {
-  COMPREPLY=($(find "$HOME/.kube/" -type f -name "${COMP_WORDS[1]}*" -printf "%f\n"))
-}
-
-complete -F _kube_context_completions kube-context
