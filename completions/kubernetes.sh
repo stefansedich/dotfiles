11bb6eefb94e2bd6a0f2de5b1c1d4b47cb5ae1b6
Stefan Sedich <stefan.sedich@gmail.com>
2020-12-12 11:15:40 -0800

    Delete kubernetes.sh

--- completions/kubernetes.sh
+++
@@ -1,7 +0,0 @@
-# Comletion helper for the kube-context command.
-_kube_context_completions() {
-  COMPREPLY=($(find "$HOME/.kube/" -type f -name "${COMP_WORDS[1]}*" -printf "%f\n"))
-}
-
-complete -F _kube_context_completions kube-context
-complete -F _kube_context_completions kc
