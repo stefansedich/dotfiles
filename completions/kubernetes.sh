e49a9c1ad3463a40b4e19961a8485e8cf226f417
Stefan Sedich <stefan.sedich@gmail.com>
2020-12-12 11:11:03 -0800

    Update kubernetes.sh

--- completions/kubernetes.sh
+++ completions/kubernetes.sh
@@ -3,5 +3,5 @@ _kube_context_completions() {
   COMPREPLY=($(find "$HOME/.kube/" -type f -name "${COMP_WORDS[1]}*" -printf "%f\n"))
 }
 
-compdef _kube_context_completions kube-context
-compdef _kube_context_completions kc
+complete -F _kube_context_completions kube-context
+complete -F _kube_context_completions kc
