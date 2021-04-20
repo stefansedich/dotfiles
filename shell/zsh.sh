0aa5f6eab40086c07e6fa771ce28e68012890128
Stefan Sedich <stefan.sedich@gmail.com>
2021-04-19 20:14:54 -0700

    Update zsh.sh

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -2,7 +2,7 @@ if [ -n "$ZSH_VERSION" ]; then
   test -e "$HOME/.zsh/completion.zsh" && source "$HOME/.zsh/completion.zsh"
   test -e "$HOME/.zsh/history.zsh" && source "$HOME/.zsh/history.zsh"
   test -e "$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" && source "$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
-  test -e "$HOME/.zsh/zsh-autosuggestions/zsh-history-substring-search.zsh" && source "$HOME/.zsh/zsh-autosuggestions/zsh-history-substring-search.zsh"
+  test -e "$HOME/.zsh/zsh-autosuggestions/zsh-history-substring-search.zsh" && source "$HOME/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh"
   test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
   
   # Setup key bindings
