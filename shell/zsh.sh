8570ae223f2ba209e39c5d0cf24b0fb2f71d75f4
Stefan Sedich <stefan.sedich@gmail.com>
2022-10-02 20:38:33 -0700

    Update zsh.sh

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -10,5 +10,20 @@ if [ -n "$ZSH_VERSION" ]; then
   bindkey "$terminfo[kcuu1]" history-substring-search-up
   bindkey "$terminfo[kcud1]" history-substring-search-down
   
+  # enable color support of ls and also add handy aliases
+  if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+  fi
+
+  # some more ls aliases
+  alias ll='ls -alF'
+  alias la='ls -A'
+  alias l='ls -CF'
+  
   eval "$(starship init zsh)"
 fi
