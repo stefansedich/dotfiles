3bc2fd5dae0090505d4fd42259e009cc6999799f
Stefan Sedich <stefan.sedich@gmail.com>
2026-02-12 11:50:55 -0800

    fix: nope

--- shell/asdf.sh
+++ shell/asdf.sh
@@ -1,18 +1,5 @@
 export ASDF_DATA_DIR="$HOME/.asdf"
 export PATH="$ASDF_DATA_DIR/shims:$PATH"
 
-# Completion paths
-ASDF_COMPLETIONS_DIR=${ASDF_DATA_DIR:-$HOME/.asdf}/completions
-[[ -d $ASDF_COMPLETIONS_DIR ]] && fpath=($ASDF_COMPLETIONS_DIR $fpath)
-
-# Cache location
-ZCOMPDUMP=${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompdump
-
-autoload -Uz compinit
-
-# Rebuild cache if missing or older than any completion file
-if [[ ! -f $ZCOMPDUMP || $ZCOMPDUMP -ot ${(j: :)fpath} ]]; then
-  compinit -d "$ZCOMPDUMP"
-else
-  compinit -C -d "$ZCOMPDUMP"
-fi
+fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
+autoload -Uz compinit && compinit
